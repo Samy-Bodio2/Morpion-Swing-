76d870dfd416daaa2f879cb48084e0287fcdd1a7
Samy-Bodio2 <samybodio8@gmail.com>
2023-03-22 05:49:06 +0100

    Morpion

---
+++ src/javaapplication45/JavaApplication45.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication45;
+
+/**
+ *
+ * @author PERSO
+ */
+public class JavaApplication45 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame v = new NewJFrame();
+        v.setVisible(true);
+    }
+    
+}
